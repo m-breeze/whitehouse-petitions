77b44287e01822e17a703543d0a3c0dbbe219d19
Marina Khort <maryna.khort@gmail.com>
2020-08-22 01:14:50 +0300

    Finished the app

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UITableViewController {
 
 	var petitions = [Petition]()
+	var filteredPetitions = [Petition]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
+		let filter = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+		let reload = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reloadTapped))
+		navigationItem.leftBarButtonItems = [reload, filter]
+		
 		let urlString: String
 		
 		if navigationController?.tabBarItem.tag == 0 {
@@ -39,17 +44,18 @@ class ViewController: UITableViewController {
 		
 		if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
 			petitions = jsonPetitions.results
+			filteredPetitions = petitions
 			tableView.reloadData()
 		}
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return petitions.count
+		return filteredPetitions.count
 	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-		let petition = petitions[indexPath.row]
+		let petition = filteredPetitions[indexPath.row]
 		cell.textLabel?.text = petition.title
 		cell.detailTextLabel?.text = petition.body
 		return cell
@@ -57,9 +63,41 @@ class ViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		let vc = DetailViewController()
-		vc.detailItem = petitions[indexPath.row]
+		vc.detailItem = filteredPetitions[indexPath.row]
 		navigationController?.pushViewController(vc, animated: true)
 	}
+	
+	@objc func creditsTapped() {
+		let ac = UIAlertController(title: "", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "Ok", style: .default))
+		present(ac, animated: true)
+	}
+	
+	@objc func filterTapped() {
+		let ac = UIAlertController(title: "", message: "What do you want to find?", preferredStyle: .alert)
+		ac.addTextField()
+		
+		let filterAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] action in
+			guard let text = ac?.textFields?[0].text else { return }
+			self?.filter(text)
+//			self?.tableView.reloadData()
+		}
+		ac.addAction(filterAction)
+		present(ac, animated: true)
+	}
+	
+	@objc func reloadTapped() {
+		filteredPetitions = petitions
+		tableView.reloadData()
+	}
+	
+	
+	func filter(_ text: String) {
+		filteredPetitions = filteredPetitions.filter {$0.body.contains(text)}
+		tableView.reloadData()
+		return
+	}
+	
 	
 	func showError() {
 		let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
